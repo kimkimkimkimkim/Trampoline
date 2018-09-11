da00a894d9f2d8a902a3523d4591c18d905de291
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2018-09-12 00:57:13 +0900

    【機能追加】trampolineの生成と背景の動き
    ・画面をなぞってtrampolineを生成
    ・背景を常に移動
    ・trampolineとボールは反発

---
+++ Trampoline/Assets/Scripts/GameScene/drawPhysicsLine.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class drawPhysicsLine : MonoBehaviour
+{
+
+	public GameObject line;
+	public GameObject linePrefab;
+	public float lineLength = 0.2f;
+	public float lineWidth = 0.1f;
+
+	private Vector3 touchPos;
+	private int count = 0;
+	private bool canDraw = false;
+
+	void Start(){
+
+	}
+
+	void Update (){
+		drawLine ();
+	}
+
+	void drawLine(){
+
+		if(Input.GetMouseButtonDown(0))
+		{
+			touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			touchPos.z=0;
+		}
+
+		if(Input.GetMouseButton(0))
+		{
+
+			Vector3 startPos = touchPos;
+			Vector3 endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			endPos.z=0;
+
+			if((endPos-startPos).magnitude > lineLength){
+				if(canDraw)Destroy (line.transform.Find ("Line" + count.ToString ()).gameObject);
+				GameObject obj = Instantiate(linePrefab, transform.position, transform.rotation) as GameObject;
+				obj.name = "Line" + count.ToString();
+				obj.transform.position = (startPos+endPos)/2;
+				obj.transform.right = (endPos-startPos).normalized;
+				obj.transform.localScale = new Vector3( (endPos-startPos).magnitude, lineWidth , lineWidth );
+				obj.transform.parent = line.transform;
+
+				canDraw = true;
+			}
+		}
+
+		if (Input.GetMouseButtonUp (0)) {
+			count++;
+			canDraw = false;
+		}
+	}
+}
